bcf04ace0c6dc3a2f2a9d3aa5f50d6084d2b56a9
Jason <eric870615@yahoo.com.tw>
2021-01-21 13:30:29 +0800

    add logo cube

--- ClickProject/click.js
+++ ClickProject/click.js
@@ -2,157 +2,15 @@ function a (){let new_element=document.createElement("script");
 new_element.setAttribute("type","text/javascript"); 
 new_element.setAttribute("src","personsketch.js");// 在這裡引入了a.js 
 document.body.appendChild(new_element); 
-// console.log(new_element.setup());
 }
-a();
-console.log(a);
-// import * as sketchModule from './sketch.js';
-
-
-// -----------------  up fail !!!! -----------------------  
-
-// let video;
-// let poseNet;
-// let poses = [];
-// let skeletons = [];
-// let detectionPersonNumber = 1;
-// let leftElbowPos = [0,0]; // number 7 leftElbow
-// let leftWristPos = [0,0]; // number 9 leftWrist
-// let timeStampTemp = Date.now();
-// let actionState = false;
-// let waveHandTime = Date.now();
-// let waveTimes = 0;
-// let waveCountState = false;
-// let playAnimation = false;
-
-
-// function setup() {
-//     createCanvas(640, 480);
-//     video = createCapture(VIDEO);
-//     video.size(width, height);
-//     poseNet = ml5.poseNet(video, 
-//       {
-//         imageScaleFactor: 0.3,
-//         outputStride: 16,
-//         flipHorizontal: false,
-//         minConfidence: 0.5,
-//         maxPoseDetections: 5,
-//         scoreThreshold: 0.5,
-//         nmsRadius: 20,
-//         detectionType: 'single',
-        
-//        }
-//       , modelReady);
-//     poseNet.on('pose', gotPoses);
-//     video.hide();
-// }
-
-// function modelReady() {
-//     //select('#status').html('Model Loaded');
-// }
-
-// function draw() {
-//     //image(video, 0, 0, width, height);
-//     drawKeypoints();
-//     // drawSkeleton();
-// }
-
-
-// function drawKeypoints()  {
-//     for (let i = 0; i < poses.length; i++) {
-//         //poses.length is number of detect people
-        
-
-//         // if( i< detectionPersonNumber){
-//         if(false){
-//             for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
-//                 let keypoint = poses[i].pose.keypoints[j];
-//                 // drow keypoint when score > 0.2
-//                 if (keypoint.score > 0.2) {
-//                     fill(255, 0, 0);
-//                     noStroke();
-//                     ellipse(keypoint.position.x, keypoint.position.y, 5, 5);
-//                 }
-//             }
-//         }
-
-//         // number 7 leftElbow
-//         let leftElbowKeypoint = poses[i].pose.keypoints[7];
-//         if (leftElbowKeypoint.score > 0.5) {
-//             leftElbowPos[0]=Math.floor(leftElbowKeypoint.position.x);
-//             leftElbowPos[1]=Math.floor(leftElbowKeypoint.position.y);
-//         }
-//         // number 9 leftElbow
-//         let leftWristKeypoint = poses[i].pose.keypoints[9];
-//         if (leftWristKeypoint.score > 0.5) {
-//             leftWristPos[0]=Math.floor(leftWristKeypoint.position.x);
-//             leftWristPos[1]=Math.floor(leftWristKeypoint.position.y);
-//         }
-//         //console.log('left elbow position : ',leftElbowPos);
-//         //console.log('left wrist position : ',leftWristPos);
-//         if(Math.abs(leftWristPos[0]-leftElbowPos[0])<30){
-//             if (!actionState && waveCountState){
-//                 waveTimes += 1;
-//                 waveCountState = false;
-//             }
-//             actionState = true;
-            
-//         }else{
-//             if (actionState && waveCountState){
-//                 waveTimes += 1;
-//                 waveCountState = false;
-//             }
-//             actionState = false;
-//             timeStampTemp = Date.now();
-//         }
-//         // console.log(waveTimes);
-//         if ((Date.now() - timeStampTemp)>0.3){
-//             waveCountState = true;
-//         }
-
-//         if ((Date.now() - timeStampTemp)>1){
-//             // console.log('the elbow and wrist is vertical state ! , distance : ',Math.abs(leftWristPos[0]-leftElbowPos[0]));
-//             timeStampTemp = Date.now();
-//         }
-
-//         if (waveTimes>7){
-//             playAnimation = true;
-//             console.log('Stacy say hello to you!!');
-//             waveTimes = 0;
-//         }else{
-//             playAnimation = false;
-//         }
-//     }
-// }
-
-// function drawSkeleton() {
-//     for (let i = 0; i < poses.length; i++) {
-//     // for (let i = 0; i < 2; i++) {
-//         if( i< detectionPersonNumber){
-//             for (let j = 0; j < poses[i].skeleton.length; j++) {
-//                 let partA = poses[i].skeleton[j][0];
-//                 let partB = poses[i].skeleton[j][1];
-//                 stroke(255, 0, 0);
-//                 line(partA.position.x, partA.position.y, partB.position.x, partB.position.y);
-//             }
-//         }
-//     }
-// }
-
-// function gotPoses(results) {
-//     poses = results;
-// }
-
-
-
+// a();
+// console.log(a);
 
 
  
 // -------------------------------------------------------------------
 //
-//
-//                down is model 
-//
+//                         down is model 
 //
 // -------------------------------------------------------------------
 
@@ -178,6 +36,7 @@ import { OrbitControls } from './import/OrbitControls.js';
         loaderAnim = document.getElementById('js-loader');
 
     let actionName = [];
+    let mouseSate = false;
 
     const mouse = new THREE.Vector2();
     const target = new THREE.Vector2();
@@ -187,14 +46,23 @@ import { OrbitControls } from './import/OrbitControls.js';
     
 
     let createCube = function(){
-        let geometry = new THREE.BoxGeometry(1,1,1);
-        let material = new THREE.MeshBasicMaterial({color:0x0f00ff,wireframe:true});
-        cube = new THREE.Mesh(geometry, material);
-        cube.position.y = 2 ;
-        cube.rotation.x = -0.2;
-        cube.rotation.y = -0.1;
-        cube.rotation.z = -0.3;
-        scene.add(cube);
+        // let geometry = new THREE.BoxGeometry(1,1,1);
+        // let material = new THREE.MeshBasicMaterial({color:0x0f00ff,wireframe:true});
+        // cube = new THREE.Mesh(geometry, material);
+        // cube.position.y = 2 ;
+        // cube.rotation.x = -0.2;
+        // cube.rotation.y = -0.1;
+        // cube.rotation.z = -0.3;
+        // scene.add(cube);
+
+        var texture = new THREE.TextureLoader().load( './man.jpg' );
+        var texture2 = new THREE.TextureLoader().load( './itri.jpg' );
+        var geometry = new THREE.BoxBufferGeometry( 2, 2, 2 );
+        var material = new THREE.MeshBasicMaterial( { map: texture2 } );
+        // geometry.faces[i].materialIndex
+        cube = new THREE.Mesh( geometry, material );
+        cube.position.y = 4 ;
+        scene.add( cube );
     };
 
     
@@ -222,7 +90,7 @@ import { OrbitControls } from './import/OrbitControls.js';
             1000
         );
         camera.position.z = 15 
-        camera.position.x = 0;
+        camera.position.x = 0; 
         camera.position.y = -3;
 
         const controls = new OrbitControls( camera, renderer.domElement );
@@ -321,10 +189,6 @@ import { OrbitControls } from './import/OrbitControls.js';
             }
         );
         
-
-
-
-
         // Add lights
         let hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.31);
         hemiLight.position.set(0, 50, 0);
@@ -376,18 +240,21 @@ import { OrbitControls } from './import/OrbitControls.js';
         //window.addEventListener( "click", onMouseMove, false );
         //window.addEventListener( 'resize', onWindowResize, false );
         //window.addEventListener( "click", onDocumentMouseMove, false );
+        window.addEventListener( "keydown", onDocumentKeyDown, false );
         document.addEventListener('mousemove', function(e) {
+            
             var mousecoords = getMousePos(e);
             if (neck && waist) {
-                //moveJoint(mousecoords, neck, 80);
+                moveJoint(mousecoords, neck, 50);
                 moveJoint(mousecoords, waist, 30);
             };
+            if(mouseSate){
             if (arm && arm2 && waist){
-                //moveJoint(mousecoords, waist, 50);
-                moveJoint(mousecoords, arm, 30);
-                moveJoint(mousecoords, arm2, 30);
+                moveJoint(mousecoords, waist, 50);
+                moveJoint(mousecoords, arm, 80);
+                moveJoint(mousecoords, arm2, 50);
                 //moveJoint(mousecoords, LeftUpLeg, 30)
-            };
+            };}
         });
 
         window.addEventListener('click', e => raycast(e));
@@ -515,7 +382,68 @@ import { OrbitControls } from './import/OrbitControls.js';
         return needResize;
     };
 
-    
+    function onDocumentKeyDown(event){
+        // 0: "pockets"
+        // 1: "rope"
+        // 2: "swingdance"
+        // 3: "jump"
+        // 4: "react"
+        // 5: "shrug"
+        // 6: "wave"
+        // 7: "golf"
+        if(event.keyCode === 96){
+            // key 96 -> 0
+            playModifierAnimation(idle, 0.25, possibleAnims[0], 0.25);
+        }
+        if(event.keyCode === 97){
+            // key 97 -> 1
+            playModifierAnimation(idle, 0.25, possibleAnims[1], 0.25);
+        }
+        if(event.keyCode === 98){
+            // key 98 -> 2
+            playModifierAnimation(idle, 0.25, possibleAnims[2], 0.25);
+        }
+        if(event.keyCode === 99){
+            // key 99 -> 3
+            playModifierAnimation(idle, 0.25, possibleAnims[3], 0.25);
+        }
+        if(event.keyCode === 100){
+            // key 99 -> 4
+            playModifierAnimation(idle, 0.25, possibleAnims[4], 0.25);
+        }
+        if(event.keyCode === 101){
+            // key 99 -> 5
+            playModifierAnimation(idle, 0.25, possibleAnims[5], 0.25);
+        }
+        if(event.keyCode === 102){
+            // key 99 -> 6
+            playModifierAnimation(idle, 0.25, possibleAnims[6], 0.25);
+        }
+        if(event.keyCode === 103){
+            // key 99 -> 7
+            playModifierAnimation(idle, 0.25, possibleAnims[7], 0.25);
+        }
+        if(event.keyCode === 90){
+            // key 90 -> z
+            mouseSate = true;
+        }
+        if(event.keyCode === 88){
+            // key 88 -> x
+            mouseSate = false;
+        }
+    }
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -529,11 +457,6 @@ import { OrbitControls } from './import/OrbitControls.js';
 
     }
 
-
-    
-
-
-
     let mainLoop = function(){
 
         if (resizeRendererToDisplaySize(renderer)) {
@@ -546,15 +469,11 @@ import { OrbitControls } from './import/OrbitControls.js';
             mixer.update(clock.getDelta());
         };
 
-
-
         // play the animations
         // console.log(playAnimation);
         // if(playAnimation){
         //     playOnPoseNet();
         // };
-
-        
 
         cube.rotation.x -= 0.05;
         cube.rotation.y -= 0.01;
